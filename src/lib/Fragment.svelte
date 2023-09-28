aed4c92344eb29562edf09e9dda457917cb117d5
Mitchel <mitchel_jager@hotmail.com>
2023-09-28 19:57:02 +0200

    feat: Add fragment component

---
+++ src/lib/Fragment.svelte
@@ -0,0 +1,26 @@
+<script>
+  import * as Svelte from 'svelte'
+
+  export let context = null
+  export let onMount = null
+  export let beforeUpdate = null
+  export let afterUpdate = null
+  export let onDestroy = null
+
+  if (context) {
+    Object.keys(context).forEach((key) => {
+      Svelte.setContext(key, context[key])
+    })
+  }
+
+  if (onMount) Svelte.onMount(bind(onMount))
+  if (beforeUpdate) Svelte.beforeUpdate(bind(beforeUpdate))
+  if (afterUpdate) Svelte.afterUpdate(bind(afterUpdate))
+  if (onDestroy) Svelte.onDestroy(bind(onDestroy))
+
+  function bind(callback) {
+    return () => callback({ props: $$restProps })
+  }
+</script>
+
+<slot />
